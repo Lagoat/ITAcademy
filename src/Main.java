8cc300af720fbca46fba7bf79e268a2a0aaf6f0d
Elina <lagoat@yandex.ru>
2020-10-24 15:17:06 +0300

    Checked how an increment works

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         Scanner in = new Scanner(System.in);
-        int x = in.nextInt();
-        int y = in.nextInt();
-        System.out.println("Сумма " + (x + y));
+        //int x = in.nextInt();
+        //int y = in.nextInt();
+        //System.out.println("Сумма " + (x + y));
+        int a = 5, b, c;
+        c = ++a;
+        b = a++;
+        System.out.println(b + " , " + c + " , " + a);
     }
 }
