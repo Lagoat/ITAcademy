ef285d6cf3e8eb7e53c21c63b0ff6737b7234b6a
Elina <lagoat@yandex.ru>
2020-10-29 19:28:22 +0300

    made a tidy A+B solution

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
         Scanner in = new Scanner(System.in);
-        //int x = in.nextInt();
-        //int y = in.nextInt();
-        //System.out.println("Сумма " + (x + y));
-        int a = 5, b, c;
-        c = ++a;
-        b = a++;
-        System.out.println(b + " , " + c + " , " + a);
+        int x = in.nextInt();
+        int y = in.nextInt();
+        System.out.print(x + y);
     }
 }
