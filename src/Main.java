a5a0a599e6432c2056d3f53795dc717a32030a8a
Elina <lagoat@yandex.ru>
2020-10-29 20:21:55 +0300

    solved extra task 1.2

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-
-        int h = n / 3600 % 24;
-        int m = n / 60 % 60;
-        int s = n % 60;
-        System.out.print(h + ":");
-        if (m / 10 == 0)
-            System.out.print("0" + m + ":");
-        else
-            System.out.print(m + ":");
-        if (s / 10 == 0)
-            System.out.print("0" + s);
-        else
-            System.out.print(s);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.print(a * a + b * b);
     }
 }
