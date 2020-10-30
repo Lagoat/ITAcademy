ff50f6bbaeb1f6aa3a0668af37826c4163debebf
Elina <lagoat@yandex.ru>
2020-10-30 22:36:55 +0300

    solved task 1.4

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        System.out.print(a * a + b * b);
+        int N = in.nextInt();
+        if (((N - 1) & N) == 0)
+            System.out.print("YES");
+        else
+            System.out.print("NO");
     }
 }
