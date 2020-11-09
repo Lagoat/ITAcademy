afea6723dc2a8854becaad9137e1d88d263707e6
Elina <lagoat@yandex.ru>
2020-11-09 18:58:09 +0300

    task 1.5.1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        if (((N - 1) & N) == 0)
-            System.out.print("YES");
-        else
-            System.out.print("NO");
+    Scanner in = new Scanner (System.in);
+    double N = in.nextDouble();
+    if (N >= 3 && N <= 8){
+        System.out.print("true");
+    }
+    else
+        System.out.print("false");
     }
 }
