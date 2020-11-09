fbd2902d105aa3f82eb02b21c455fd5ac2ffd837
Elina <lagoat@yandex.ru>
2020-11-09 19:13:39 +0300

    task 1.5.5

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Scanner in = new Scanner (System.in);
-    int N = in.nextInt();
-    if ((N % 5 == 0) && (N > 99 && N < 1000)){
-        System.out.print("true");
-    }
-    else
-        System.out.print("false");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        if ((a == -b) || (a == -c) || (a == -d)) {
+            System.out.print("true");
+        } else if ((b == -c) || (b == -d)) {
+            System.out.print("true");
+        } else if (c == -d) {
+            System.out.print("true");
+        } else
+            System.out.print("false");
     }
 }
