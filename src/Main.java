aac69d54007ea0e99b98c1587ab1a74f2e58d83e
Elina <lagoat@yandex.ru>
2020-11-09 19:05:21 +0300

    task 1.5.4

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     Scanner in = new Scanner (System.in);
-    double N = in.nextDouble();
-    if ((N >= -2 && N <= 3) || (N >= 6 && N <= 9)){
-        System.out.print("false");
+    int N = in.nextInt();
+    if ((N % 5 == 0) && (N > 99 && N < 1000)){
+        System.out.print("true");
     }
     else
-        System.out.print("true");
+        System.out.print("false");
     }
 }
