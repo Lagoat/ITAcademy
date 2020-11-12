b26f4d81903b2383d4764e1f37adea32349e75ea
Elina <lagoat@yandex.ru>
2020-11-12 16:37:21 +0300

    t1.7.2

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int x = in.nextInt();
-        int c = 1;
-        while (x >= 0) {
-            c++;
+        int sum = 0;
+        if (x > 10)
+            sum += x;
+        while (x % 5 != 0)
+        {
             x = in.nextInt();
+            if (x > 10)
+                sum += x;
         }
-        System.out.print(c);
+        System.out.print(sum);
     }
 }
