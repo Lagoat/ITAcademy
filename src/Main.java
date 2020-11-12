8115f7141dfcf476e9a0b3736d71c7df2582b24f
Elina <lagoat@yandex.ru>
2020-11-12 16:56:49 +0300

    t1.7.8

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-        int amnt = 0;
+        int dig = 0;
         while (N > 0) {
-            amnt++;
+            if ((N % 10) % 2 != 0)
+            {
+                dig = N % 10;
+                break;
+            }
             N /= 10;
         }
-        System.out.print(amnt);
+        if (dig == 0)
+            System.out.print("NO");
+
+        else
+            System.out.print(dig);
     }
 }
