43184eb5414fb8e692277c1c47014aa1a251d53b
Elina <lagoat@yandex.ru>
2020-11-12 18:29:23 +0300

    t1.8.3

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,26 @@
 import javax.swing.*;
 //-1,5 2,8 NO
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int sum = -1;
+        double sum = 0, amnt = 0, aref = 0;
         int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             a[i] = in.nextInt();
-            if (a[i] % 2 == 0)
-                sum = 0;
+            if (a[i] % 2 != 0) {
+                amnt++;
+                sum += a[i];
+            }
         }
-        if (sum == -1)
+        if (amnt == 0)
             System.out.print("NO");
         else {
-            for (int i = 0; i < n; i++)
-                if (a[i] % 2 == 0)
-                    sum += a[i];
-            System.out.print(sum);
+            aref = sum / amnt;
+            String answer = new DecimalFormat("#0.00").format(aref);
+            System.out.print(answer);
         }
     }
 }
