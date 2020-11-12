302ede8acb5ac86b7434f1b9c5096c40ae58a91f
Elina <lagoat@yandex.ru>
2020-11-12 19:02:31 +0300

    t1.9.1

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,17 @@
 import javax.swing.*;
 //-1,5 2,8 NO
-import java.util.Scanner;
 import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
+    public static int sum(int n) {
+        n = n / 10 + n % 10;
+        return n;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int neg = 0;
-        int[] a = new int[n];
-        int[] b = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            a[i] = in.nextInt();
-            if (a[i] < 0) {
-                b[neg] = a[i];
-                neg++;
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            if (a[i] >= 0)
-            {
-                b[neg] = a[i];
-                neg++;
-            }
-            System.out.print(b[i] + " ");
-        }
-
+        System.out.print(sum(n));
     }
 }
