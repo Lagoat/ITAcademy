03b9b350bf4a291085afe49a68474c23dd6f5038
Elina <lagoat@yandex.ru>
2020-11-12 19:25:47 +0300

    t1.9.4

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,21 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
-    public static int amount(int n) {
-        int i = 100;
-        int amnt = 0;
-        while (i < n) {
-            if ((i % 10 + i / 100 + i / 10 % 10) % 13 == 0)
-                amnt++;
-            i++;
+    public static String bin(double x, int n) {
+        String ans = "";
+        for (int i = 0; i < n; i++) {
+            x *= 2;
+            ans += (int)x;
+            if ((int)x == 1)
+                x -= 1;
         }
-        return amnt;
+        return ans;
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        double x = in.nextDouble();
         int n = in.nextInt();
-        System.out.print(amount(n));
+        System.out.print(bin(x, n));
     }
 }
