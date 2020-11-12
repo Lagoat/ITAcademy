cb553967bb4ace856ff021d9beac6112a5f90242
Elina <lagoat@yandex.ru>
2020-11-12 18:31:58 +0300

    t1.8.4

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        double sum = 0, amnt = 0, aref = 0;
         int[] a = new int[n];
-        for (int i = 0; i < n; i++){
-            a[i] = in.nextInt();
-            if (a[i] % 2 != 0) {
-                amnt++;
-                sum += a[i];
-            }
-        }
-        if (amnt == 0)
-            System.out.print("NO");
-        else {
-            aref = sum / amnt;
-            String answer = new DecimalFormat("#0.00").format(aref);
-            System.out.print(answer);
+
+        for (int i = 0; i < n; i++) {
+            a[i] = 4 + 3 * i;
+            System.out.print(a[i] + " ");
         }
     }
 }
