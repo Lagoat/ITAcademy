b6700233b39e4ca58910aa4a19d446c7f3293958
Elina <lagoat@yandex.ru>
2020-11-12 15:17:35 +0300

    unfinished task 1.6.4

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int mod = a % 100;
-        if (mod / 10 == 1){
-            System.out.print(a + " " + "TOPTOB");
+        double A = in.nextDouble();
+        double B = in.nextDouble();
+        double x1 = B / A, x2 = -B / A;
+
+        if (A == 0)
+            System.out.print("no such x");
+        else if (A > 0) {
+            if (x1 > -B && x2 > -B)
+                System.out.print("any x");
+            else if (x1 > x2)
+                System.out.print("x<" + x2 + " or x>" + x1);
+            else
+                System.out.print("x<" + x1 + " or x>" + x2);
         }
         else {
-            if (mod % 10 == 1)
-                System.out.print(a + " " + "TOPT");
-            else if (mod % 10 >= 2 && mod % 10 <= 4)
-                System.out.println(a + " " + "TOPTA");
+            if (x1 < -B && x2 < -B)
+                System.out.print("any x");
+            else if (x1 > x2)
+                System.out.print(x2 + "<x<" + x1);
             else
-                System.out.println(a + " " + "TOPTOB");
+                System.out.print(x1 + "<x<" + x2);
         }
+
     }
 }
