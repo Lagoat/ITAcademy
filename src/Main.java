41dd931be3d43db30a25cdd2ab7ab17bc73f6567
Elina <lagoat@yandex.ru>
2020-11-12 18:41:55 +0300

    t1.8.6

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
+        int moda = 0, max_moda = 0, index = -1;
         int[] a = new int[n];
 
         for (int i = 0; i < n; i++)
             a[i] = in.nextInt();
-        for (int i = 0; i < n / 2; i++)
-        {
-            int t = a[i];
-            a[i] = a[n - 1 - i];
-            a[n - 1 - i] = t;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i] == a[j])
+                    moda++;
+            }
+            if (moda > max_moda){
+                index = i;
+                max_moda = moda;
+            }
+            moda = 0;
         }
-        for (int i = 0; i < n; i++)
-            System.out.print(a[i] + " ");
+        System.out.print(a[index]);
     }
 }
