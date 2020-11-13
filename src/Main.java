317e7675dbe060fd85a99f651252b399b1df25cf
Elina <lagoat@yandex.ru>
2020-11-13 22:46:56 +0300

    Xt1.7.2

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int h = 437;
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int moda = Math.abs(a);
+        int modb = Math.abs(b);
+        int mult = 0;
         int i = 0;
-        int flag = 0;
-        while (i != n) {
-            i++;
-            int h1 = in.nextInt();
-            if (h1 <= h) {
-                System.out.print("Crash " + i);
-                flag++;
-                break;
-            }
+        if (a == 0 || b == 0) {
+            System.out.print(mult);
         }
-        if (flag == 0)
-            System.out.print("No crash");
+        else {
+            while (i != moda) {
+                i++;
+                mult += modb;
+            }
+            if (a < 0 && b < 0 || a > 0 && b > 0)
+                System.out.print(mult);
+            else
+                System.out.print(-1 * mult);
+        }
     }
 }
