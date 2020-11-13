ed65f63be1897d831b477d1094a9ea0e4363916d
Elina <lagoat@yandex.ru>
2020-11-13 13:24:51 +0300

    Xt.1.5.8

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,13 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
-    public static void aver(int[] a)
-    {
-        Scanner in = new Scanner(System.in);
-        int amnt = 0; int sum = 0;
-        for(int i = 0; i < a.length; i++)
-        {
-            if (a[i] >= 1000 && a[i] <= 9999) {
-                sum += a[i];
-                amnt++;
-            }
-        }
-        if (amnt == 0)
-            System.out.print(-1.00);
-        else
-            System.out.print(sum/amnt);
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] a = new int [n];
-        for (int i = 0; i < n; i++)
-            a[i] = in.nextInt();
-        aver(a);
+        double x = in.nextDouble();
+        double y = in.nextDouble();
+        if ((y != x - 1 && y != 1 && x * x + y * y < 1 && x > 0) || (y < 1 && y > x - 1 && x > 0))
+            System.out.print("YES");
+        else
+            System.out.print("NO");
     }
 }
