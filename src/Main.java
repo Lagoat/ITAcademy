99b0817db7f7061aea18cb181afe6b175a6aba98
Elina <lagoat@yandex.ru>
2020-11-13 20:27:41 +0300

    t1.11.1

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,23 @@
-import javax.swing.*;
-//-1,5 2,8 NO
 import java.text.DecimalFormat;
 import java.util.Scanner;
-
 public class Main {
-    public static void t6(int[] a, int n) {
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            if (a[i] > max)
-                max = a[i];
-        }
-        int min = max + 1;
-        int min_i = -1;
-        for (int i = 0; i < n; i++) {
-            if (a[i] < min) {
-                min = a[i];
-                min_i = i;
-            }
-        }
-        for (int i = min_i; i > 0; i--) {
-            int t = a[i];
-            a[i] = a[i - 1];
-            a[i - 1] = t;
-        }
-        for (int i = 0; i < n; i++)
-            System.out.print(a[i] + " ");
-    }
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] a  = new int [n];
-        for (int i = 0; i < n; i++)
-            a[i] = in.nextInt();
-        t6(a,n);
+        int m = in.nextInt();
+        int[][] a = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                a[i][j] = in.nextInt();
+        }
+        int[][] b = new int[m][n];
+        System.out.println(m + " " + n);
+        for(int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                b[i][j] = a[n - j - 1][i];
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
