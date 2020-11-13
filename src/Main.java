633024b91f5e72f2cf7c96cd301cb89562570437
Elina <lagoat@yandex.ru>
2020-11-13 14:29:23 +0300

    Xt1.6.2

--- src/Main.java
+++ src/Main.java
@@ -6,36 +6,83 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double A = in.nextDouble();
-        double B = in.nextDouble();
-        DecimalFormat df = new DecimalFormat("###.#");
-        if (A > 0 && B > 0)
-            System.out.print("any x");
-        else if (A == 0 && B > 0)
-            System.out.print("any x");
-        else if (A > 0 && B == 0)
-            System.out.print("any x");
-        else if (A == 0 && B == 0)
-            System.out.print("no such x");
-        else if (A < 0 && B < 0)
-            System.out.print("no such x");
-        else if (A == 0 && B < 0)
-            System.out.print("no such x");
-        else if (A < 0 && B == 0)
-            System.out.print("no such x");
-        else if (A < 0 && B > 0) {
-            double x1 = -B/A;
-            double x2 = -B/(-A);
-            String x1f = String.format("%.1f", x1);
-            String x2f = String.format("%.1f", x2);
-            System.out.printf(x2f + "<x<" + x1f);
-        }
-        else if (A > 0 && B < 0) {
-            double x1 = -B/A;
-            double x2 = -B/(-A);
-            String x1f = String.format("%.1f", x1);
-            String x2f = String.format("%.1f", x2);
-            System.out.printf("x<" + x2f + " or x>" + x1f);
+        int month = in.nextInt();
+        int day = in.nextInt();
+        switch(month) {
+            case 1:
+                if (day > 31 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day);
+                break;
+            case 2:
+                if (day > 28 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day - 31);
+                break;
+            case 3:
+                if (day > 31 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day - 59);
+                break;
+            case 4:
+                if (day > 30 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day - 90);
+                break;
+            case 5:
+                if (day > 31 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day - 120);
+                break;
+            case 6:
+                if (day > 30 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day - 151);
+                break;
+            case 7:
+                if (day > 31 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day - 181);
+                break;
+            case 8:
+                if (day > 31 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day - 212);
+                break;
+            case 9:
+                if (day > 30 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day - 243);
+                break;
+            case 10:
+                if (day > 31 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day - 273);
+                break;
+            case 11:
+                if (day > 30 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day - 304);
+                break;
+            case 12:
+                if (day > 31 || day < 1)
+                    System.out.print(-1);
+                else
+                    System.out.print(365 - day - 334);
+                break;
+            default:
+                System.out.print(-1);
         }
     }
 }
