577dd1b05ceb8d37e6de6bf67e87575c4788f4b8
Elina <lagoat@yandex.ru>
2020-11-13 21:44:40 +0300

    t1.11.4

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int check = 0;
-        int[][] a = new int[n][n];
-        int[][] b = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                a[i][j] = in.nextInt();
+        int m = in.nextInt();
+        int[][] a = new int [n][m];
+        int i = 0, j = 0;
+        while (i < n && j < m) {
+            a[i][j] = i * j;
+            System.out.printf("%4d", a[i][j]);
+            j++;
+            if(j == m) {
+                i++;
+                j = 0;
+                System.out.println("");
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                b[i][j] = a[j][i];
-                if (b[i][j] == a[i][j])
-                    check++;
-            }
-        }
-
-        if (check == n*n)
-            System.out.print("yes");
-        else
-            System.out.print("no");
     }
 }
