4069861d88d5bd8444467d3426cba9fc5163a2b4
Elina <lagoat@yandex.ru>
2020-11-13 19:51:04 +0300

    t1.9.5

--- src/Main.java
+++ src/Main.java
@@ -4,62 +4,30 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
+    public static void t5(int[] a, int n) {
+        double sum = 0;
+        double amnt = 0;
+        for (int i = 0; i < n; i++) {
+                 if (a[i] >= 1000 && a[i] <= 9999) {
+                     sum += a[i];
+                     amnt++;
+                 }
+             }
+        if (amnt == 0)
+            System.out.printf("-1.00");
+        else {
+            String ans = String.format("%.2f", (sum / amnt));
+            System.out.printf(ans);
+        }
+
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int num = in.nextInt();
-        String rom = "";
-        if (num == 100) {
-            num = -1;
-            rom += "C";
-        }
-        if (num >= 90) {
-            num -= 90;
-            rom += "XC";
-        }
-        if (num >= 50) {
-            num -= 50;
-            rom += "L";
-        }
-        if (num >= 40) {
-            num -= 40;
-            rom += "XL";
-        }
-        if (num >= 30) {
-            num -= 30;
-            rom += "XXX";
-        }
-        if (num >= 20) {
-            num -= 20;
-            rom += "XX";
-        }
-        if (num >= 10) {
-            num -= 10;
-            rom += "X";
-        }
-        if (num >= 9) {
-            num -= 9;
-            rom += "IX";
-        }
-        if (num >= 5) {
-            num -= 5;
-            rom += "V";
-        }
-        if (num >= 4) {
-            num -= 4;
-            rom += "IV";
-        }
-        if (num >= 3) {
-            num -= 3;
-            rom += "III";
-        }
-        if (num >= 2) {
-            num -= 2;
-            rom += "II";
-        }
-        if (num >= 1) {
-            num -= 1;
-            rom += "I";
-        }
-        System.out.print(rom);
+        int n = in.nextInt();
+        int[] a  = new int [n];
+        for (int i = 0; i < n; i++)
+            a[i] = in.nextInt();
+        t5(a, n);
     }
 }
