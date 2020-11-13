d2c35f874a022d0ea673248a36556bc429df1a2f
Elina <lagoat@yandex.ru>
2020-11-13 20:48:49 +0300

    t1.11.2

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int m = in.nextInt();
-        int[][] a = new int[n][m];
+        int[][] a = new int[n][n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++)
-                a[i][j] = in.nextInt();
-        }
-        int[][] b = new int[m][n];
-        System.out.println(m + " " + n);
-        for(int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                b[i][j] = a[n - j - 1][i];
-                System.out.print(b[i][j] + " ");
+                if (j == n - i - 1)
+                    a[i][j] = 1;
+                else if (i < n - j - 1 && j < n - i - 1)
+                    a[i][j] = 0;
+                else
+                    a[i][j] = 2;
+                System.out.print(a[i][j] + " ");
             }
             System.out.println("");
         }
