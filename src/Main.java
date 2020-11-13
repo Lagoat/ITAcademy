886861086a619a73acf394c13b3c42b9691ce18c
Elina <lagoat@yandex.ru>
2020-11-13 15:53:55 +0300

    Xt1.6.3

--- src/Main.java
+++ src/Main.java
@@ -6,83 +6,60 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int month = in.nextInt();
-        int day = in.nextInt();
-        switch(month) {
-            case 1:
-                if (day > 31 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day);
-                break;
-            case 2:
-                if (day > 28 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day - 31);
-                break;
-            case 3:
-                if (day > 31 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day - 59);
-                break;
-            case 4:
-                if (day > 30 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day - 90);
-                break;
-            case 5:
-                if (day > 31 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day - 120);
-                break;
-            case 6:
-                if (day > 30 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day - 151);
-                break;
-            case 7:
-                if (day > 31 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day - 181);
-                break;
-            case 8:
-                if (day > 31 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day - 212);
-                break;
-            case 9:
-                if (day > 30 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day - 243);
-                break;
-            case 10:
-                if (day > 31 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day - 273);
-                break;
-            case 11:
-                if (day > 30 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day - 304);
-                break;
-            case 12:
-                if (day > 31 || day < 1)
-                    System.out.print(-1);
-                else
-                    System.out.print(365 - day - 334);
-                break;
-            default:
-                System.out.print(-1);
+        int num = in.nextInt();
+        String rom = "";
+        if (num == 100) {
+            num = -1;
+            rom += "C";
         }
+        if (num >= 90) {
+            num -= 90;
+            rom += "XC";
+        }
+        if (num >= 50) {
+            num -= 50;
+            rom += "L";
+        }
+        if (num >= 40) {
+            num -= 40;
+            rom += "XL";
+        }
+        if (num >= 30) {
+            num -= 30;
+            rom += "XXX";
+        }
+        if (num >= 20) {
+            num -= 20;
+            rom += "XX";
+        }
+        if (num >= 10) {
+            num -= 10;
+            rom += "X";
+        }
+        if (num >= 9) {
+            num -= 9;
+            rom += "IX";
+        }
+        if (num >= 5) {
+            num -= 5;
+            rom += "V";
+        }
+        if (num >= 4) {
+            num -= 4;
+            rom += "IV";
+        }
+        if (num >= 3) {
+            num -= 3;
+            rom += "III";
+        }
+        if (num >= 2) {
+            num -= 2;
+            rom += "II";
+        }
+        if (num >= 1) {
+            num -= 1;
+            rom += "I";
+        }
+        System.out.print(rom);
     }
 }
