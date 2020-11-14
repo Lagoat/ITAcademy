6f8996b61e61101589f5f5583565ee9d49296a0f
Elina <lagoat@yandex.ru>
2020-11-14 08:18:47 +0300

    Xt(1)1.8.4

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++)
-            a[i] = 0;
-        for (int i = 0; i < n - 1; i++) {
-            int x = in.nextInt();
-            a[x - 1] = 1;
-        }
+        int[] f = new int[n];
+        int ans = 1;
         for (int i = 0; i < n; i++) {
-            if (a[i] == 0) {
-                System.out.print(i + 1);
-                break;
-            }
+            f[i] = i + 1;
+            ans *= f[i];
         }
+        System.out.print(ans);
     }
 }
