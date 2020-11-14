a7372f4ea5fdf528698c2ec04f453020f86a768f
Elina <lagoat@yandex.ru>
2020-11-14 09:07:41 +0300

    Xt1.9.2

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
 public class Main {
-    public static double l(int x1, int y1, int x2, int y2) {
-        double x = x2 - x1;
-        double y = y2 - y1;
-        return Math.sqrt(x*x + y*y);
+    public static int min (int a, int b, int c, int d){
+        int arr[] = new int[] {a, b, c, d};
+        int max = -1;
+        for (int i = 0; i < 4; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        int min = max + 1;
+        for (int i = 0; i < 4; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+        }
+        return min;
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int x1 = in.nextInt();
-        int y1 = in.nextInt();
-        int x2 = in.nextInt();
-        int y2 = in.nextInt();
-        int x3 = in.nextInt();
-        int y3 = in.nextInt();
-        System.out.printf("%.10f", (l(x1, y1, x2, y2) + l(x2, y2, x3, y3) + l(x3, y3, x1, y1)));
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        System.out.print(min(a,b,c,d));
     }
 }
