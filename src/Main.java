d7573048b8c861737f406cbd143460df7268c10d
Elina <lagoat@yandex.ru>
2020-11-14 08:07:07 +0300

    Xt(1)1.8.1

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double a = in.nextDouble();
-        double a1 = 1;
-        long n = in.nextLong();
-        long i = 0;
-        if (n % 2 == 0) {
-            while (i != n/2) {
-                a1 *= a;
-                i++;
-            }
-            System.out.print(a1 * a1);
+        int n = in.nextInt();
+        while (n != 0) {
+            System.out.print(n % 2);
+            n /= 2;
         }
-        else {
-            while (i != n - 1) {
-                a1 *= a;
-                i++;
-            }
-            System.out.print(a * a1);
-        }
-
     }
 }
