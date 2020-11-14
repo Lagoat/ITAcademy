cc14192515c550885a825847f3c72a77b479509a
Elina <lagoat@yandex.ru>
2020-11-14 08:35:03 +0300

    Xt(2)1.8.2

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] f = new int[n];
-        int ans = 1;
+        int[] a = new int[n];
         for (int i = 0; i < n; i++) {
-            f[i] = i + 1;
-            ans *= f[i];
+            a[i] = in.nextInt();
+            if (i % 2 == 0)
+                System.out.print(a[i] + " ");
         }
-        System.out.print(ans);
     }
 }
