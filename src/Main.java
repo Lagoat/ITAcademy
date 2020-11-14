b1ceedd9c0724b40062fb3b40d9f1e7a770f4e3b
Elina <lagoat@yandex.ru>
2020-11-14 08:51:25 +0300

    Xt(2)1.8.7

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,14 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] a = new int[n];
-        int min = 500;
+        int amnt = 1;
         for (int i = 0; i < n; i++) {
             a[i] = in.nextInt();
-            if (a[i] < min)
-                min = a[i];
+            if (i > 0)
+                if (a[i] != a[i - 1])
+                    amnt++;
         }
-        int max = min;
-        for (int i = 0; i < n; i++) {
-            if (a[i] > max)
-                max = a[i];
-        }
-        System.out.print(max);
+
+        System.out.print(amnt);
     }
 }
