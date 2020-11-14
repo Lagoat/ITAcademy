eeaebf3c0e1aaf496e231d4fa38b557001c67184
Elina <lagoat@yandex.ru>
2020-11-14 09:15:56 +0300

    Xt(1)1.8.3

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,17 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
 public class Main {
-    public static int min (int a, int b, int c, int d){
-        int arr[] = new int[] {a, b, c, d};
-        int max = -1;
-        for (int i = 0; i < 4; i++) {
-            if (arr[i] > max)
-                max = arr[i];
-        }
-        int min = max + 1;
-        for (int i = 0; i < 4; i++) {
-            if (arr[i] < min)
-                min = arr[i];
-        }
-        return min;
-    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
-        int d = in.nextInt();
-        System.out.print(min(a,b,c,d));
+        int n = in.nextInt();
+        int next = 0;
+        int[] a = new int[n * n];
+        for (int i = 0; i < n * n; i++) {
+            a[i] = i + 1;
+            if (i == next) {
+                System.out.print(a[i] + " ");
+                next += n + 1;
+            }
+        }
     }
 }
